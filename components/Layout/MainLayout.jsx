1fb738b5e04672d27f43cadbe4e7a24ef34af01d
Your Name <you@example.com>
2025-12-28 16:57:18 +0200

    change background color

--- components/Layout/MainLayout.jsx
+++ components/Layout/MainLayout.jsx
@@ -2,11 +2,11 @@ import Header from "./Header";
 import Footer from "./Footer";
 import { Layout } from "antd";
 export default function MainLayout({ children }) {
-   return (
-      <Layout style={{ margin: '0 auto'}}>
-         <Header />
-         <Layout.Content>{children}</Layout.Content>
-         <Footer />
-      </Layout>
-   );
+  return (
+    <Layout style={{ margin: "0 auto", background: "white" }}>
+      <Header />
+      <Layout.Content>{children}</Layout.Content>
+      <Footer />
+    </Layout>
+  );
 }

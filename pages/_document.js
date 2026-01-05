c205f1c95f894687841ffaab4a6e6278c6bb2f29
Momen-Elshamy <eng.momen.adel@gmail.com>
2026-01-05 20:11:26 +0100

    Revamp footer component with enhanced layout, social links, and contact information; update document stylesheets for UI icons.

--- pages/_document.js
+++ pages/_document.js
@@ -4,9 +4,13 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-      <link
+<link
   rel="stylesheet"
   href="https://cdn-uicons.flaticon.com/3.0.0/uicons-regular-rounded/css/uicons-regular-rounded.css"
+/>
+<link
+  rel="stylesheet"
+  href="https://cdn-uicons.flaticon.com/2.1.0/uicons-brands/css/uicons-brands.css"
 />
       </Head>
       <body>

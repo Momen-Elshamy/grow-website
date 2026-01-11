984551982dae725b8e24b70b6b0d415045fcb1d4
Mayar Hassan <mayarmohamed775@gmail.com>
2026-01-11 20:25:42 +0200

    refactor code

--- components/Layout/InfoHeader.jsx
+++ components/Layout/InfoHeader.jsx
@@ -1,76 +1,116 @@
 import { Row, Col } from "antd";
 import Uicons from "../UI/Uicons";
 import styles from "./InfoHeader.module.css";
+
+const socialIcons = [
+   { icon: "fi-brands-twitter", desktopSize: 20, mobileSize: 18 },
+   { icon: "fi-brands-facebook", desktopSize: 20, mobileSize: 18 },
+   { icon: "fi-brands-pinterest", desktopSize: 20, mobileSize: 18 },
+   { icon: "fi-brands-instagram", desktopSize: 20, mobileSize: 18 },
+];
+
+const infoItems = [
+   {
+      icon: "fi-rr-phone-call",
+      text: "Call anytime",
+      info: "+98 (000) - 9630",
+      desktopSize: 30,
+      mobileSize: 40,
+   },
+   {
+      icon: "fi-rr-newsletter-subscribe",
+      text: "Send email",
+      info: "ambed@agrios.com",
+      desktopSize: 30,
+      mobileSize: 40,
+   },
+   {
+      icon: "fi-rr-land-layer-location",
+      text: "380 St Kilda Road",
+      info: "Melbourne, Australia",
+      desktopSize: 30,
+      mobileSize: 40,
+   },
+];
+
 export default function InfoHeader() {
-    return (
-       <div className={styles.infoHeader} >
-           <Row gutter={16} justify="center" className={styles.desktopRow}>
-             {/* Icons Side */}
-             <Col span={12} className={`${styles.iconsContainer} ${styles.iconsContainerLeft}`}>
-             <div className={styles.iconsWrapper}>
-             <div className={styles.iconBox} ><Uicons icon="fi-brands-twitter" size={20} color="black" className={styles.socialIcon} /></div>
-             <div className={styles.iconBox} ><Uicons icon="fi-brands-facebook" size={20} color="black" className={styles.socialIcon} /></div>
-             <div className={styles.iconBox} ><Uicons icon="fi-brands-pinterest" size={20} color="black" className={styles.socialIcon} /></div>
-             <div className={styles.iconBox} ><Uicons icon="fi-brands-instagram" size={20} color="black" className={styles.socialIcon} /></div>
-             </div>
-             <div className={styles.infoText}>
-                <Uicons icon="fi-rr-phone-call" size={30} color="#107634" className={styles.infoIcon}/>
-                <div className={styles.infoTextContent}>
-                   <p className={styles.text}> Call anytime </p>
-                   <p className={styles.info}> +98 (000) - 9630 </p>
-                </div>
-             </div>
-             </Col>
-             {/* Right Side */}
-             <Col span={6} className={styles.iconsContainer}>
-             <div className={styles.infoText}>
-                <Uicons icon="fi-rr-newsletter-subscribe" size={30} color="#107634" className={styles.infoIcon}/>
-                <div className={styles.infoTextContent}>
-                   <p className={styles.text}> Send email </p>
-                   <p className={styles.info}> ambed@agrios.com </p>
-                </div>
-             </div>
-             </Col>
-             <Col span={6} className={styles.iconsContainer}>
-             <div className={styles.infoText}>
-                <Uicons icon="fi-rr-land-layer-location" size={30} color="#107634" className={styles.infoIcon} />
-                <div className={styles.infoTextContent}>
-                   <p className={styles.text}> 380 St Kilda Road </p>
-                   <p className={styles.info}> Melbourne, Australia </p>
-                </div>
-             </div>
-             </Col>
-          </Row>
-          {/* Mobile Layout */}
-          <div className={styles.mobileLayout}>
-             <div className={styles.mobileInfoItem}>
-                <Uicons icon="fi-rr-phone-call" size={40} color="#107634" className={styles.infoIcon} />
-                <div className={styles.infoTextContent}>
-                   <p className={styles.text}> Call anytime </p>
-                   <p className={styles.info}> +98 (000) - 9630 </p>
-                </div>
-             </div>
-             <div className={styles.mobileInfoItem}>
-                <Uicons icon="fi-rr-newsletter-subscribe" size={40} color="#107634" className={styles.infoIcon} />
-                <div className={styles.infoTextContent}>
-                   <p className={styles.text}> Send email </p>
-                   <p className={styles.info}> ambed@agrios.com </p>
-                </div>
-             </div>
-             <div className={styles.mobileInfoItem}>
-                <Uicons icon="fi-rr-land-layer-location" size={40} color="#107634" className={styles.infoIcon} />
-                <div className={styles.infoTextContent}>
-                   <p className={styles.text}> 380 St Kilda Road </p>
-                   <p className={styles.info}> Melbourne, Australia </p>
-                </div>
-             </div>
-             <div className={styles.mobileIconsWrapper}>
-                <div className={styles.iconBox} ><Uicons icon="fi-brands-twitter" size={18} color="black" className={styles.socialIcon} /></div>
-                <div className={styles.iconBox} ><Uicons icon="fi-brands-facebook" size={18} color="black" className={styles.socialIcon} /></div>
-                <div className={styles.iconBox} ><Uicons icon="fi-brands-pinterest" size={18} color="black" className={styles.socialIcon}  /></div>
-                <div className={styles.iconBox} ><Uicons icon="fi-brands-instagram" size={18} color="black" className={styles.socialIcon} /></div>
-             </div>
-          </div>
-       </div>
-    );
+   return (
+      <div className={styles.infoHeader}>
+         <Row gutter={16} justify="center" className={styles.desktopRow}>
+            {/* Icons Side */}
+            <Col span={12} className={`${styles.iconsContainer} ${styles.iconsContainerLeft}`}>
+               <div className={styles.iconsWrapper}>
+                  {socialIcons.map((social, index) => (
+                     <div key={index} className={styles.iconBox}>
+                        <Uicons
+                           icon={social.icon}
+                           size={social.desktopSize}
+                           color="black"
+                           className={styles.socialIcon}
+                        />
+                     </div>
+                  ))}
+               </div>
+               <div className={styles.infoText}>
+                  <Uicons
+                     icon={infoItems[0].icon}
+                     size={infoItems[0].desktopSize}
+                     color="#107634"
+                     className={styles.infoIcon}
+                  />
+                  <div className={styles.infoTextContent}>
+                     <p className={styles.text}>{infoItems[0].text}</p>
+                     <p className={styles.info}>{infoItems[0].info}</p>
+                  </div>
+               </div>
+            </Col>
+            {/* Right Side */}
+            {infoItems.slice(1).map((item, index) => (
+               <Col key={index} span={6} className={styles.iconsContainer}>
+                  <div className={styles.infoText}>
+                     <Uicons
+                        icon={item.icon}
+                        size={item.desktopSize}
+                        color="#107634"
+                        className={styles.infoIcon}
+                     />
+                     <div className={styles.infoTextContent}>
+                        <p className={styles.text}>{item.text}</p>
+                        <p className={styles.info}>{item.info}</p>
+                     </div>
+                  </div>
+               </Col>
+            ))}
+         </Row>
+         {/* Mobile Layout */}
+         <div className={styles.mobileLayout}>
+            {infoItems.map((item, index) => (
+               <div key={index} className={styles.mobileInfoItem}>
+                  <Uicons
+                     icon={item.icon}
+                     size={item.mobileSize}
+                     color="#107634"
+                     className={styles.infoIcon}
+                  />
+                  <div className={styles.infoTextContent}>
+                     <p className={styles.text}>{item.text}</p>
+                     <p className={styles.info}>{item.info}</p>
+                  </div>
+               </div>
+            ))}
+            <div className={styles.mobileIconsWrapper}>
+               {socialIcons.map((social, index) => (
+                  <div key={index} className={styles.iconBox}>
+                     <Uicons
+                        icon={social.icon}
+                        size={social.mobileSize}
+                        color="black"
+                        className={styles.socialIcon}
+                     />
+                  </div>
+               ))}
+            </div>
+         </div>
+      </div>
+   );
 }
